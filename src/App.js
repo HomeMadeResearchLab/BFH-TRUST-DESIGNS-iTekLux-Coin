1e055d7657bf7e3ebe0cb7ae0a4c6492bf5acf1c
CEO <141790061+HomeMadeResearchLab@users.noreply.github.com>
2025-02-18 19:23:19 +0000

    Rebuild React App for GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>BFH Trust Designs - React App</h1>
+      <p>Welcome to the deployed version of your project!</p>
+    </div>
+  );
+}
+
+export default App;
