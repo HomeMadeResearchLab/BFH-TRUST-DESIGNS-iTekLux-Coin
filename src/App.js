be0d2dc250f03ea1524f367b92128e6517fc1b9d
CEO <141790061+HomeMadeResearchLab@users.noreply.github.com>
2025-02-20 03:21:47 +0000

    Added new build, public, and src files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>BFH Trust Designs - React App</h1>
-      <p>Welcome to the deployed version of your project!</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to My App</h1>
+            <p>This is a basic React setup.</p>
+        </div>
+    );
 }
 
 export default App;
